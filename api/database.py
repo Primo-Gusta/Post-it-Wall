6fc976e0612926208998dedfecbeba95b3634a57
Primo-Gusta <guguneri1106@gmail.com>
2026-02-09 08:40:11 -0300

    feat/ changes on compose and database to accomodate Dockerfile

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://user:password@localhost:5432/postit_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postit_admin:password@localhost:5432/postit_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
